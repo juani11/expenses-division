f47358e2b907d07bfd2c3a80916bbcbcb59127d5
juani11 <juanitagliero@gmail.com>
2023-03-07 19:01:58 -0300

    google fonts, saco default css de index.css,  agrego config colors y fontFamily en tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-      ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}   
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                // primary: '#039BE5',
+                primary: '#6c63ff',
+                secondary: '#2f2e41'
+            },
+            fontFamily: {
+                primary: ['"Poiret One"', 'sans-serif']
+                // primary: ['lato', 'sans-serif']
+            }
+        }
+    },
+    plugins: []
+}
