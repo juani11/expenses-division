69ea05e9012b668f3327d083a3c5cb89800a777b
juani11 <juanitagliero@gmail.com>
2023-03-11 12:21:47 -0300

    agrgeo prop size en Button

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,31 +1,21 @@
-const Button = ({ children, loading = false }) => {
+import LoadingSVG from '../svg/LoadingSVG'
+
+const buttonSizes = {
+    xs: 'text-sm h-2',
+    sm: 'h-4',
+    md: 'h-13',
+    lg: 'h-8'
+}
+
+const Button = ({ children, size = 'md', loading = false }) => {
     const loadingClassNames = loading ? 'bg-gray-500' : 'bg-secondary'
+
+    const buttonSize = buttonSizes[size]
     return (
         <button
-            className={`uppercase mt-9 font-bold text-white py-4 px-6 hover:bg-gray-600 flex justify-center items-center ${loadingClassNames}`}
+            className={`uppercase font-bold text-white py-4 px-6 hover:bg-gray-600 flex justify-center items-center ${loadingClassNames} ${buttonSize}`}
         >
-            {loading && (
-                <svg
-                    className='w-5 h-5 mr-3 -ml-1 text-white animate-spin'
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                >
-                    <circle
-                        className='opacity-25'
-                        cx='12'
-                        cy='12'
-                        r='10'
-                        stroke='currentColor'
-                        strokeWidth='4'
-                    ></circle>
-                    <path
-                        className='opacity-75'
-                        fill='currentColor'
-                        d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-                    ></path>
-                </svg>
-            )}
+            {loading && <LoadingSVG />}
             <div>{children}</div>
         </button>
     )
