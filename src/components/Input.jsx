ba5b4da4b3001c9e99351117657d004356f49a41
juani11 <juanitagliero@gmail.com>
2023-03-11 12:18:30 -0300

    error state en Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,23 @@
-const Input = ({ name, width }) => {
-    const error = false
+import { useState } from 'react'
+
+const Input = ({ label, name, width }) => {
+    const [inputValue, setInputValue] = useState('')
+    const [error, setError] = useState(false)
+
+    const handleChange = e => {
+        const { value } = e.target
+        if (value === 'hola') {
+            setError(true)
+        } else {
+            setError(false)
+        }
+        setInputValue(value)
+    }
 
     return (
-        <div className='flex flex-col gap-2 '>
-            <label htmlFor='name' className='text-md text-gray-800 '>
-                {name}
+        <div className='flex flex-col'>
+            <label htmlFor='name' className='text-md text-gray-800 mb-2 '>
+                {label}
             </label>
             <input
                 className={`bg-white border text-xl rounded-md ${
@@ -12,8 +25,11 @@ const Input = ({ name, width }) => {
                         ? 'text-red-500 border-red-500 bg-red-50'
                         : ' text-black border-black'
                 } h-14 px-5 `}
-                name='name'
+                name={name}
+                value={inputValue}
+                onChange={handleChange}
             />
+            {error && <p className='text-red-500 '>Please enter Note</p>}
         </div>
     )
 }
