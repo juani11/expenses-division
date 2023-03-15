30521c1f4d24758c7c002a4aec0eacb653e613ea
juani11 <juanitagliero@gmail.com>
2023-03-15 08:38:33 -0300

    close y menu svg components

---
+++ src/components/svg/CloseSVG.jsx
@@ -0,0 +1,26 @@
+const CloseSVG = () => {
+    return (
+        <svg
+            part='svg'
+            xmlns='http://www.w3.org/2000/svg'
+            aria-hidden='true'
+            aria-labelledby='ic-close'
+            focusable='false'
+            viewBox='0 0 24 24'
+            width='25'
+            height='25'
+            className='h-icon icon-gray icon-static-view-box h-icon--no-custom-width referral-banner__close-icon cursor-pointer'
+            // dataV21f970be=''
+        >
+            <g>
+                <path
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    d='M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z'
+                ></path>
+            </g>
+        </svg>
+    )
+}
+
+export default CloseSVG
---
+++ src/components/svg/MenuSVG.jsx
@@ -0,0 +1,27 @@
+const MenuSVG = () => {
+    return (
+        <svg
+            part='svg'
+            xmlns='http://www.w3.org/2000/svg'
+            aria-hidden='true'
+            aria-labelledby='actionDots'
+            focusable='false'
+            viewBox='0 0 24 24'
+            className='h-icon icon-primary cursor-pointer d-none d-md-flex'
+            width='20'
+            height='20'
+            // dataV68e5bb38=''
+            // block=''
+        >
+            <g>
+                <g>
+                    <circle cx='12.1091' cy='4' r='2'></circle>
+                    <circle cx='12.1091' cy='20' r='2'></circle>
+                    <circle cx='12.1091' cy='12' r='2'></circle>
+                </g>
+            </g>
+        </svg>
+    )
+}
+
+export default MenuSVG
