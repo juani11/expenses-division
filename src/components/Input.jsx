6a23b6dc64e530dbdc7b3ee5f0850984f8584ff9
juani11 <juanitagliero@gmail.com>
2023-03-16 08:07:07 -0300

    agrego prop controlledProps a Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-
-const Input = ({ label, name, width }) => {
-    const [inputValue, setInputValue] = useState('')
-    const [error, setError] = useState(false)
-
-    const handleChange = e => {
-        const { value } = e.target
-        if (value === 'hola') {
-            setError(true)
-        } else {
-            setError(false)
-        }
-        setInputValue(value)
-    }
-
+const Input = ({ label, name, width, className = '', controlledProps, error }) => {
     return (
         <div className='flex flex-col'>
             <label htmlFor='name' className='text-md text-gray-800 mb-2 '>
                 {label}
             </label>
             <input
-                className={`bg-white border text-xl rounded-md ${
-                    error
-                        ? 'text-red-500 border-red-500 bg-red-50'
-                        : ' text-black border-black'
+                className={`bg-white border text-xl rounded-md focus:border-red-50 ${
+                    error ? 'text-red-500 border-red-500 bg-red-50' : ' text-black border-black'
                 } h-14 px-5 `}
-                name={name}
-                value={inputValue}
-                onChange={handleChange}
+                {...controlledProps}
             />
-            {error && <p className='text-red-500 '>Please enter Note</p>}
+            {error && <p className='text-red-500 '>{error.message}</p>}
         </div>
     )
 }
