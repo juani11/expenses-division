8dc702226abeb24b80771bb46ea5891fa9d63dea
juani11 <juanitagliero@gmail.com>
2023-04-07 19:43:53 -0300

    modifico el svg de arrow por el svg de heroicons

--- src/components/svg/ArrowSVG.jsx
+++ src/components/svg/ArrowSVG.jsx
@@ -1,16 +1,15 @@
 const ArrowSVG = () => {
     return (
         <svg
-            width='24'
-            height='24'
             xmlns='http://www.w3.org/2000/svg'
-            fillRule='evenodd'
-            clipRule='evenodd'
+            viewBox='0 0 24 24'
+            fill='currentColor'
+            className='w-6 h-6 text-primary'
         >
             <path
-                d='M21.883 12l-7.527 6.235.644.765 9-7.521-9-7.479-.645.764 7.529 6.236h-21.884v1h21.883z'
-                // className='bg-primary'
-                fill='#6c63ff'
+                fillRule='evenodd'
+                d='M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z'
+                clipRule='evenodd'
             />
         </svg>
     )
