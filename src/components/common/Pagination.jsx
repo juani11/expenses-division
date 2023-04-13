50d3ebf3daeac5be32d799e76a1898a4d4355001
juani11 <juanitagliero@gmail.com>
2023-04-13 11:03:41 -0300

    agrego svg icons en Pagination component

--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -1,15 +1,20 @@
+import ChevronLeft from '../svg/ChevronLeft'
+import ChevronRight from '../svg/ChevronRight'
+
 const Pagination = ({ cantPages, currentPage, prevPage, nextPage, changeCurrentPage }) => {
     return (
         <div className='flex justify-end items-center gap-2 mt-2'>
             {currentPage > 1 && (
-                <button className='px-3 py-1' onClick={prevPage}>
-                    {'<'}
+                <button className='px-3 py-1 rounded-md hover:bg-gray-100' onClick={prevPage}>
+                    <ChevronLeft />
                 </button>
             )}
             {[...Array(cantPages)].map((_, index) => (
                 <button
                     key={index}
-                    className={`px-3 py-1 ${index + 1 === currentPage && 'bg-gray-200'} rounded-md`}
+                    className={`px-3 py-1 rounded-md hover:bg-gray-100 ${
+                        index + 1 === currentPage && 'bg-gray-100 font-bold'
+                    }  `}
                     onClick={changeCurrentPage}
                     value={index + 1}
                 >
@@ -18,8 +23,8 @@ const Pagination = ({ cantPages, currentPage, prevPage, nextPage, changeCurrentP
             ))}
             <div className='w-8'>
                 {currentPage < cantPages && (
-                    <button className='px-3 py-1' onClick={nextPage}>
-                        {'>'}
+                    <button className='px-3 py-1 rounded-md hover:bg-gray-100' onClick={nextPage}>
+                        <ChevronRight />
                     </button>
                 )}
             </div>
