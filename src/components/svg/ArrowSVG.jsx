c0e9e8065397b3c340850c51eef830f531919a5e
juani11 <juanitagliero@gmail.com>
2023-04-17 10:54:34 -0300

    nuevos SVG, modifico ArrowSVG

--- src/components/svg/ArrowSVG.jsx
+++ src/components/svg/ArrowSVG.jsx
@@ -4,14 +4,27 @@ const ArrowSVG = () => {
             xmlns='http://www.w3.org/2000/svg'
             viewBox='0 0 24 24'
             fill='currentColor'
-            className='w-6 h-6 text-primary'
+            className='w-8 h-8 text-primary'
         >
             <path
                 fillRule='evenodd'
-                d='M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z'
+                d='M16.72 7.72a.75.75 0 011.06 0l3.75 3.75a.75.75 0 010 1.06l-3.75 3.75a.75.75 0 11-1.06-1.06l2.47-2.47H3a.75.75 0 010-1.5h16.19l-2.47-2.47a.75.75 0 010-1.06z'
                 clipRule='evenodd'
             />
         </svg>
+
+        // <svg
+        //     xmlns='http://www.w3.org/2000/svg'
+        //     viewBox='0 0 24 24'
+        //     fill='currentColor'
+        //     className='w-8 h-8 text-primary'
+        // >
+        //     <path
+        //         fillRule='evenodd'
+        //         d='M3.75 12a.75.75 0 01.75-.75h13.19l-5.47-5.47a.75.75 0 011.06-1.06l6.75 6.75a.75.75 0 010 1.06l-6.75 6.75a.75.75 0 11-1.06-1.06l5.47-5.47H4.5a.75.75 0 01-.75-.75z'
+        //         clipRule='evenodd'
+        //     />
+        // </svg>
     )
 }
 
