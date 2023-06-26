63994e392608a71c906cbdb90800937c3bc98b4c
juani11 <juanitagliero@gmail.com>
2023-06-26 17:17:21 -0300

    refactoring y cambio de estilo DivisionList

--- src/components/group/DivisionsList.jsx
+++ src/components/group/DivisionsList.jsx
@@ -1,7 +1,7 @@
-import { calcularResultadoFinal } from '../../logic/logic'
+import { calcularCantidadADarYRecibir, calcularResultadoFinal } from '../../logic/logic'
 import { useGroupStore } from '../../store/store'
-import { currencyFormat, toFloat } from '../../utils/utils'
 import CardHeader from '../common/CardHeader'
+import MoneyAmount from '../common/MoneyAmount'
 import Skeleton from '../common/Skeleton/Skeleton'
 import SkeletonWrapper from '../common/Skeleton/SkeletonWrapper'
 import ArrowSVG from '../svg/ArrowSVG'
@@ -20,44 +20,7 @@ const DivisionsList = () => {
     const expenses = useGroupStore(state => state.expenses)
     const loading = useGroupStore(state => state.loading)
 
-    // const includedPersonsInExpense = useGroupStore(state => state.includedPersonsInExpense)
-    // const personIsIncludedInExpense = useGroupStore(state => state.personIsIncludedInExpense)
-
-    let cantidadesADar = []
-    let cantidadesARecibir = []
-
-    const personIsIncludedInExpense = (person, includedPersons) => {
-        includedPersons.includes(person)
-    }
-    const calcularCantidadADarYRecibir = () => {
-        persons.forEach(person => {
-            let total = 0
-            expenses.forEach(expense => {
-                const { amount, person: owner, includedPersons } = expense
-
-                // Si la persona actual esta incluida en el gasto actual, sumo la division por persona del gasto actual en la persona
-
-                if (personIsIncludedInExpense(person.id, includedPersons)) {
-                    // const includedPersons = includedPersonsInExpense(excludedPersons)
-                    total = toFloat(total + amount / includedPersons.length)
-                }
-                // Si la persona actual es dueña del gasto actual resto el valor en la persona
-                if (owner === person.id) {
-                    total = toFloat(total - amount)
-                }
-            })
-
-            if (total > 0) {
-                cantidadesADar = [...cantidadesADar, { person, amount: total }]
-                cantidadesARecibir = [...cantidadesARecibir, { person, amount: 0 }]
-            } else {
-                cantidadesADar = [...cantidadesADar, { person, amount: 0 }]
-                cantidadesARecibir = [...cantidadesARecibir, { person, amount: total * -1 }]
-            }
-        })
-    }
-
-    calcularCantidadADarYRecibir()
+    const { cantidadesADar, cantidadesARecibir } = calcularCantidadADarYRecibir(persons, expenses)
     const resultados = calcularResultadoFinal(cantidadesADar, cantidadesARecibir)
 
     return (
@@ -67,22 +30,24 @@ const DivisionsList = () => {
             {loading ? (
                 <DivisionsLoading />
             ) : (
-                <ul className='shadow-lg bg-white py-5 px-2  rounded-xl '>
+                <ul className=' py-5 px-2 rounded-xl '>
                     {resultados.map((division, index) => (
-                        <li
-                            key={index}
-                            className='flex justify-between items-center mb-4 hover:bg-gray-50 p-3'
-                        >
-                            <div className='flex gap-1 items-center'>
-                                <ArrowSVG />
-                                <div className='flex flex-col items-center '>
-                                    <h5 className='m-0'>{division.personaFrom.name}</h5>
-                                    <h5 className='m-0'>{division.personaTo.name}</h5>
-                                </div>
+                        <li key={index} className='shadow bg-white rounded mb-3 hover:bg-gray-50 p-4'>
+                            <div className='flex items-center '>
+                                <h5 className='m-0 uppercase  text-gray-400'>{division.personaFrom.name}</h5>
                             </div>
 
-                            <div className='bg-primary-300 w-24 rounded-2xl text-primary'>
-                                <h4 className='m-2 text-center'>{currencyFormat(division.cantidad)}</h4>
+                            <div className='flex justify-between items-center'>
+                                <div className='px-1 rounded-md'>
+                                    <ArrowSVG />
+                                </div>
+                                <h5 className='m-0 uppercase w-20 text-gray-400'>
+                                    {division.personaTo.name}
+                                </h5>
+                                <MoneyAmount
+                                    amount={division.cantidad}
+                                    className='bg-primary text-white font-bold'
+                                />
                             </div>
                         </li>
                     ))}
@@ -93,3 +58,26 @@ const DivisionsList = () => {
 }
 
 export default DivisionsList
+
+// {/* <li
+// key={index}
+// className='flex justify-between items-center mb-3 hover:bg-gray-50 p-3'
+// >
+// <h5 className='m-0 capitalize w-10 text-gray-400'>{division.personaFrom.name}</h5>
+// <div className='flex flex-col items-center '>
+//     {/* <ArrowSVG /> */}
+//     <h4 className='m-0'></h4>
+//     <h4 className='m-0 relative max-w-fit bg-avatar3 rounded-xl text-white text-center py-2 pl-3  '>
+//         <span className='absolute text-xs top-2'>$</span>
+//         <span className='px-3'>{`${currencyFormat(division.cantidad)}`}</span>
+//     </h4>
+// </div>
+
+// <h5 className='m-0 capitalize w-10  text-gray-400'>{division.personaTo.name}</h5>
+
+// {/* <ExpenseCost cost={division.cantidad} /> */}
+
+// {/* <div className='bg-primary-300 w-24 rounded-2xl text-primary'>
+//     <h4 className='m-2 text-center'>{currencyFormat(division.cantidad)}</h4>
+// </div> */}
+// </li> */}
