81233ba48937027601de57da0e13a723e56a4f30
juani11 <juanitagliero@gmail.com>
2023-08-08 15:19:13 -0300

    elimino propiedad "id" a cada constante de cada mes

--- src/constants.js
+++ src/constants.js
@@ -8,67 +8,56 @@ const JANUARY = {
     shortName: 'ene'
 }
 const FEBRUARY = {
-    id: 1,
     number: 2,
     name: 'febrero',
     shortName: 'feb'
 }
 const MARCH = {
-    id: 2,
     number: 3,
     name: 'marzo',
     shortName: 'mar'
 }
 const APRIL = {
-    id: 3,
     number: 4,
     name: 'abril',
     shortName: 'abr'
 }
 const MAY = {
-    id: 4,
     number: 5,
     name: 'mayo',
     shortName: 'may'
 }
 const JUNE = {
-    id: 5,
     number: 6,
     name: 'junio',
     shortName: 'jun'
 }
 const JULY = {
-    id: 6,
     number: 7,
     name: 'julio',
     shortName: 'jul'
 }
 const AUGUST = {
-    id: 7,
     number: 8,
     name: 'agosto',
     shortName: 'ago'
 }
 const SEPTEMBER = {
-    id: 8,
     number: 9,
     name: 'septiembre',
     shortName: 'sep'
 }
 const OCTOBER = {
-    id: 9,
     number: 10,
     name: 'octubre',
     shortName: 'oct'
 }
 const NOVEMBER = {
-    id: 10,
     number: 11,
     name: 'noviembre',
     shortName: 'nov'
 }
 const DECEMBER = {
-    id: 11,
     number: 12,
     name: 'diciembre',
     shortName: 'dic'
