aea7b578d9f6029860b40b4dee949111a9ae730a
juani11 <juanitagliero@gmail.com>
2023-08-29 09:54:51 -0300

    CardContentWithMenu component

--- src/components/group/Divisions.jsx
+++ src/components/group/Divisions.jsx
@@ -1,7 +1,7 @@
-import useCardMenu from '../../hooks/useCardMenu'
+import { DIVISIONS_CASH, DIVISIONS_CREDIT } from '../../constants'
 import { useGroupStore } from '../../store/store'
+import CardContentWithMenu from '../common/CardContentWithMenu'
 import CardHeader from '../common/CardHeader'
-import CardMenu from '../common/CardMenu'
 import Skeleton from '../common/Skeleton/Skeleton'
 import SkeletonWrapper from '../common/Skeleton/SkeletonWrapper'
 import DivisionListCredit from './DivisionListCredit'
@@ -17,38 +17,23 @@ const DivisionsLoading = () => {
     )
 }
 
-export const DIVISIONS_CASH = 'efectivo'
-export const DIVISIONS_CREDIT = 'crédito'
-
 const menuItemsComponents = {
     [DIVISIONS_CASH]: DivisionsList,
     [DIVISIONS_CREDIT]: DivisionListCredit
 }
 
-const menuItems = [DIVISIONS_CASH, DIVISIONS_CREDIT]
-
 const Divisions = () => {
     const loading = useGroupStore(state => state.loading)
 
-    const { menuItemSelected, changeMenuOption, MenuItemContent } = useCardMenu(menuItemsComponents)
     return (
-        <div>
-            <section id='divisions'>
-                <CardHeader title={'divisiones'} />
-                {loading ? (
-                    <DivisionsLoading />
-                ) : (
-                    <>
-                        <CardMenu
-                            menuItems={menuItems}
-                            menuItemSelected={menuItemSelected}
-                            changeMenuOption={changeMenuOption}
-                        />
-                        <MenuItemContent />
-                    </>
-                )}
-            </section>
-        </div>
+        <section id='divisions'>
+            <CardHeader title={'divisiones'} />
+            {loading ? (
+                <DivisionsLoading />
+            ) : (
+                <CardContentWithMenu menuItemsComponents={menuItemsComponents} />
+            )}
+        </section>
     )
 }
 
--- src/components/group/Persons.jsx
+++ src/components/group/Persons.jsx
@@ -1,40 +1,23 @@
-import useCardMenu from '../../hooks/useCardMenu'
+import { PERSONS_LIST, PERSONS_TOTALS } from '../../constants'
 import { useGroupStore } from '../../store/store'
+import CardContentWithMenu from '../common/CardContentWithMenu'
 import CardHeader from '../common/CardHeader'
-import CardMenu from '../common/CardMenu'
 import PersonsList from './PersonsList'
 import PersonsLoading from './PersonsLoading'
 import PersonsTotals from './PersonsTotals'
-
-const PERSONS_LIST = 'listado'
-const PERSONS_TOTALS = 'totales'
 
 const menuItemsComponents = {
     [PERSONS_LIST]: PersonsList,
     [PERSONS_TOTALS]: PersonsTotals
 }
 
-const menuItems = [PERSONS_LIST, PERSONS_TOTALS]
-
 const Persons = () => {
     const loading = useGroupStore(state => state.loading)
 
-    const { menuItemSelected, changeMenuOption, MenuItemContent } = useCardMenu(menuItemsComponents)
     return (
         <section id='persons'>
             <CardHeader title={'personas'} />
-            {loading ? (
-                <PersonsLoading />
-            ) : (
-                <>
-                    <CardMenu
-                        menuItems={menuItems}
-                        menuItemSelected={menuItemSelected}
-                        changeMenuOption={changeMenuOption}
-                    />
-                    <MenuItemContent />
-                </>
-            )}
+            {loading ? <PersonsLoading /> : <CardContentWithMenu menuItemsComponents={menuItemsComponents} />}
         </section>
     )
 }
