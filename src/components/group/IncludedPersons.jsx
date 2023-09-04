bbc370616ad4ecced8f1b96aea0dff0b4f70fa11
juani11 <juanitagliero@gmail.com>
2023-09-04 14:22:25 -0300

    reduzco border radius

--- src/components/group/IncludedPersons.jsx
+++ src/components/group/IncludedPersons.jsx
@@ -2,11 +2,11 @@ import { EXCLUDE } from '../../store/store'
 import Button from '../common/Button'
 import PersonDetail from './PersonDetail'
 
-const IncludedPersons = ({ persons, costPerPerson, expenseId, callback }) => {
+const IncludedPersons = ({ persons, costPerPerson, expenseId, callback, type, creditTypeInfo }) => {
     return (
         <>
             <div className='flex items-center justify-between'>
-                <h5 className=' rounded-xl w-max p-2 bg-gray-50 dark:bg-slate-700'>Personas incluidas</h5>
+                <h5 className=' rounded w-max p-2 bg-gray-50 dark:bg-slate-700'>Personas incluidas</h5>
             </div>
             <ul>
                 {persons.map(includedPerson => {
@@ -16,6 +16,8 @@ const IncludedPersons = ({ persons, costPerPerson, expenseId, callback }) => {
                             person={includedPerson}
                             cost={costPerPerson}
                             expenseId={expenseId}
+                            type={type}
+                            creditTypeInfo={creditTypeInfo}
                         >
                             {(loading, onClick) => {
                                 const newIncludedPersonsInExpense = callback(EXCLUDE, includedPerson)
