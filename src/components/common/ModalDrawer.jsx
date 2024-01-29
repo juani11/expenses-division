14a58863e6fd64e40625b117793c372f444d99f5
juani11 <juanitagliero@gmail.com>
2024-01-29 15:58:32 -0300

    refactor styles ModalDrawer y ModalDrawerForm

--- src/components/common/ModalDrawer.jsx
+++ src/components/common/ModalDrawer.jsx
@@ -8,28 +8,29 @@ const ModalDrawer = ({ title, isOpen, closeModal, children }) => {
     useEffect(() => {
         document.body.style.overflow = isOpen ? 'hidden' : 'auto'
     }, [isOpen])
+
     return (
-        <>
-            <div
-                className={`fixed top-0 right-0 z-40 h-full px-4 pb-4 overflow-y-scroll transition-transform bg-white w-full md:w-[654px] dark:bg-gray-800  ${
-                    isOpen ? 'translate-x-0 shadow-2xl' : 'translate-x-full'
-                }`}
-                ref={elemRef}
-            >
-                <header className='flex justify-between items-center'>
-                    <h2 className='py-4 uppercase m-0 '>{title}</h2>
-                    <button
-                        type='button'
-                        className='text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5  items-center dark:hover:bg-gray-600 dark:hover:text-white'
-                        onClick={closeModal}
-                    >
-                        <CloseIcon color='black' />
-                    </button>
-                </header>
-                <hr />
-                <div className='grid grid-rows-[auto-fit_1fr] gap-5 relative  '>{children}</div>
+        <aside
+            className={`fixed top-0 right-0 z-40 h-full px-4 pb-4 overflow-y-scroll transition-transform bg-white w-full md:w-[654px] dark:bg-gray-800  ${
+                isOpen ? 'translate-x-0 shadow-2xl' : 'translate-x-full'
+            }`}
+            ref={elemRef}
+        >
+            <header className='flex justify-between items-center py-4 '>
+                <h2 className='text-xl uppercase m-0 '>{title}</h2>
+                <button
+                    type='button'
+                    className='text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 items-center dark:hover:bg-gray-600 dark:hover:text-white'
+                    onClick={closeModal}
+                >
+                    <CloseIcon color='black' />
+                </button>
+            </header>
+            <hr />
+            <div id='drawerContent' className='relative'>
+                {children}
             </div>
-        </>
+        </aside>
     )
 }
 
--- src/components/common/ModalDrawerForm.jsx
+++ src/components/common/ModalDrawerForm.jsx
@@ -1,44 +1,46 @@
 import { useEffect } from 'react'
 import { CloseIcon } from '../icons/icons'
 import Button from './Button'
+import useClickOutsideElement from '../../hooks/useClickOutsideElement'
 
 const ModalDrawerForm = ({ title, isOpen, closeModal, isLoading, callback, children }) => {
+    const { elemRef } = useClickOutsideElement(closeModal)
+
     useEffect(() => {
         document.body.style.overflow = isOpen ? 'hidden' : 'auto'
     }, [isOpen])
 
     return (
-        <>
-            <div
-                className={`fixed top-0 right-0 z-40 h-full p-4 overflow-y-scroll transition-transform bg-white w-full  md:w-[654px] dark:bg-gray-800  ${
-                    isOpen ? 'translate-x-0 shadow-xl' : 'translate-x-full'
-                }`}
-            >
-                <header className='flex justify-between items-center'>
-                    <h2 className='uppercase m-0 p-4'>{title}</h2>
-                    <button
-                        type='button'
-                        className='text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5  items-center dark:hover:bg-gray-600 dark:hover:text-white'
-                        onClick={closeModal}
-                    >
-                        <CloseIcon color='black' />
-                    </button>
-                </header>
-                <hr />
+        <aside
+            className={`fixed top-0 right-0 z-40 h-full px-4 pb-4 overflow-y-scroll transition-transform bg-white w-full md:w-[654px] dark:bg-gray-800  ${
+                isOpen ? 'translate-x-0 shadow-xl' : 'translate-x-full'
+            }`}
+            ref={elemRef}
+        >
+            <header className='flex justify-between items-center py-4'>
+                <h2 className='text-xl uppercase m-0 '>{title}</h2>
+                <button
+                    type='button'
+                    className='text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 items-center dark:hover:bg-gray-600 dark:hover:text-white'
+                    onClick={closeModal}
+                >
+                    <CloseIcon color='black' />
+                </button>
+            </header>
+            <hr />
 
-                <form onSubmit={callback}>
-                    {children}
-                    <footer className='flex gap-4 justify-end mt-7 p-4'>
-                        <Button type='button' onClick={closeModal} width='w-full'>
-                            cancelar
-                        </Button>{' '}
-                        <Button loading={isLoading} color='primary' width='w-full'>
-                            aceptar
-                        </Button>
-                    </footer>
-                </form>
-            </div>
-        </>
+            <form onSubmit={callback}>
+                {children}
+                <footer className='flex gap-4 justify-end mt-7 p-4'>
+                    <Button type='button' onClick={closeModal} width='w-full'>
+                        cancelar
+                    </Button>{' '}
+                    <Button loading={isLoading} color='primary' width='w-full'>
+                        aceptar
+                    </Button>
+                </footer>
+            </form>
+        </aside>
     )
 }
 
