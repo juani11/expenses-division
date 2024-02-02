3fcfaedc44f515c44881d1b56662ff6d31ca5cfc
juani11 <juanitagliero@gmail.com>
2024-02-02 10:18:19 -0300

    refactor MoneyAmount component styles for mobile

--- src/components/common/MoneyAmount.jsx
+++ src/components/common/MoneyAmount.jsx
@@ -3,9 +3,8 @@ import { formatedAmount } from '../../utils/utils'
 const MoneyAmount = ({ amount, className }) => {
     const formatedAm = formatedAmount(amount)
     return (
-        <div className={` max-w-fit text-center rounded-lg py-1.5 px-3  ${className}`}>
-            {/* <span className='absolute text-xs top-2'>$</span> */}
-            <span className='text-md '>{formatedAm}</span>
+        <div className={`max-w-fit text-center rounded-md py-1 px-2.5 md:py-1.5 md:px-3 ${className}`}>
+            <span className='text-sm md:text-base'>{formatedAm}</span>
         </div>
     )
 }
