08239959fec9424a8b0d4266edc1a7e8353e8bfa
juani11 <juanitagliero@gmail.com>
2024-06-15 13:18:31 -0300

    Update Loading.jsx

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -21,7 +21,7 @@ const Loading = ({ loadingText = 'cargando...', inline }) => {
                     d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
                 ></path>
             </svg>
-            <h5 className='m-2 capitalize '>{loadingText}</h5>
+            <h5 className='m-2 '>{loadingText}</h5>
         </div>
     )
 }
