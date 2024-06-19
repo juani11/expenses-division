db901df996c59dd903e9a5c306d2b3cd9991e1a4
juani11 <juanitagliero@gmail.com>
2024-06-19 08:36:09 -0300

    Update Modal.jsx

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -1,23 +1,23 @@
 import { useState } from 'react'
 import { CloseIcon } from '../icons/icons'
 import Button from './Button'
-const Modal = ({ isOpen, closeModal, title, callback, closable, withFooter, children }) => {
-    const [isLoading, setIsLoading] = useState(false)
-    const handleClick = () => {
-        setIsLoading(true)
-        callback().then(res => {
-            console.log({ res })
-            setIsLoading(false)
-            closeModal()
-        })
-    }
+const Modal = ({ isOpen, closeModal, title, callback, closable, withFooter, isLoading, children }) => {
+    // const [isLoading, setIsLoading] = useState(false)
+    // const handleClick = () => {
+    //     setIsLoading(true)
+    //     callback().then(res => {
+    //         console.log({ res })
+    //         setIsLoading(false)
+    //         closeModal()
+    //     })
+    // }
     return (
         <div
             className={`fixed ${
                 isOpen ? 'block' : 'hidden'
             } inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full animate-fade z-50`}
         >
-            <div className='relative top-10 mx-auto p-5 border w-fit shadow-xl rounded-md bg-white dark:bg-slate-800 dark:border-slate-700 '>
+            <div className='relative top-10 mx-auto p-5 border md:w-[565px] shadow-xl rounded-md bg-white dark:bg-slate-800 dark:border-slate-700 '>
                 {/* {closable && (
                     <div className='absolute right-2 top-2'>
                         <Button onClick={closeModal}>
@@ -41,8 +41,10 @@ const Modal = ({ isOpen, closeModal, title, callback, closable, withFooter, chil
 
                     {withFooter && (
                         <footer className='flex gap-4 items-center mt-10 py-3'>
-                            <Button onClick={closeModal}>Cancelar</Button>
-                            <Button loading={isLoading} onClick={handleClick}>
+                            <Button onClick={closeModal} width='w-full'>
+                                Cancelar
+                            </Button>
+                            <Button loading={isLoading} color='primary' onClick={callback} width='w-full'>
                                 OK
                             </Button>
                         </footer>
