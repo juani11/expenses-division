c49107ac45b7d7e8f3f40a2b0d87ab18d372cd50
juani11 <juanitagliero@gmail.com>
2024-08-30 16:25:05 -0300

    update tailwind config with primary and secondary colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,7 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     darkMode: 'class',
@@ -6,15 +9,45 @@ module.exports = {
         extend: {
             colors: {
                 // primary: '#039BE5',
+
                 primary: {
-                    DEFAULT: '#6c63ff',
-                    500: '#818cf8',
-                    300: '#d3d0ff',
-                    200: '#ECEBFF'
+                    50: '#F0F0FF',
+                    100: '#E2E0FF',
+                    200: '#C5C2FF',
+                    300: '#A8A3FF',
+                    400: '#8B85FF',
+                    DEFAULT: '#6C63FF',
+                    600: '#2A1FFF',
+                    700: '#0B00D6',
+                    800: '#07008F',
+                    900: '#040047',
+                    950: '#020024'
                 },
+
+                // primary: {
+                //     DEFAULT: '#6c63ff',
+                //     500: '#818cf8',
+                //     300: '#d3d0ff',
+                //     200: '#ECEBFF'
+                // },
+
+                // secondary: {
+                //     DEFAULT: '#2f2e41',
+                //     500: '#6b7280'
+                // },
+
                 secondary: {
-                    DEFAULT: '#2f2e41',
-                    500: '#6b7280'
+                    50: '#E7E7EE',
+                    100: '#D0CFDD',
+                    200: '#A4A2BD',
+                    300: '#75739C',
+                    400: '#504E6E',
+                    DEFAULT: '#2F2E41',
+                    600: '#252433',
+                    700: '#1C1B27',
+                    800: '#13131B',
+                    900: '#09080C',
+                    950: '#040406'
                 },
 
                 avatar1: '#ff6c63',
@@ -23,17 +56,17 @@ module.exports = {
                 avatar4: '#FF595E',
                 periwinkle: '#82C3EC'
             },
-            fontFamily: {
-                primary: ['"Poiret One"', 'sans-serif']
-                // primary: ['"Lexend"', 'sans-serif']
-                // primary: ['"Onest"', 'sans-serif']
-                // primary: ['lato', 'sans-serif']
-            },
+            // fontFamily: {
+            //     primary: ['"Poiret One"', 'sans-serif']
+            //     // primary: ['"Lexend"', 'sans-serif']
+            //     // primary: ['"Onest"', 'sans-serif']
+            //     // primary: ['lato', 'sans-serif']
+            // },
             // that is animation class
             animation: {
                 fade: 'fade 0.3s ease-in-out',
                 loading: 'loading 1.3s infinite',
-                fadeLeft: 'fadeLeft 0.3s ease-in'
+                fadeLeft: 'fadeLeft 0.4s ease-in-out'
             },
             keyframes: {
                 fade: {
@@ -46,7 +79,7 @@ module.exports = {
                     '100%': { transform: ' translateX(150%)' }
                 },
                 fadeLeft: {
-                    '0%': { opacity: 0, transform: 'translateX(3%)' },
+                    '0%': { opacity: 0, transform: 'translateX(-1%)' },
                     '100%': { transform: 'translateX(0%)', opacity: 1 }
                 }
             }
