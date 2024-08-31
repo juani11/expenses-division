157e963431e2dc82f26ec01a8309841e86a50108
juani11 <juanitagliero@gmail.com>
2024-08-31 13:56:00 -0300

    change CustomSelect styles

--- src/components/common/CustomSelect.jsx
+++ src/components/common/CustomSelect.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { ChevronDownIcon, ChevronUpIcon } from '../icons/icons'
+import { ChevronDownIcon } from '../icons/icons'
 import Label from './Label'
 import useClickOutsideElement from '../../hooks/useClickOutsideElement'
 
@@ -10,25 +10,27 @@ const types = {
     },
     form: {
         container: 'w-full',
-        button: 'h-14 border focus-within:border-1 border-gray-200 focus-within:border-primary-300  dark:border-slate-700'
+        button: 'h-9 rounded text-sm border focus-within:border-1 border-gray-200 focus-within:border-primary-300  dark:border-secondary-700'
     }
 }
 
 const Option = ({ onClick, option }) => {
     return (
         <li
-            className='cursor-pointer  select-none py-1 px-5 bg-white hover:bg-primary-300 hover:text-white dark:bg-slate-700 dark:hover:bg-slate-800 '
+            className='cursor-pointer select-none mx-1 rounded py-1.5 px-3 transition bg-white text-black hover:bg-primary-300 hover:text-white dark:bg-secondary-900 dark:hover:bg-secondary-700 dark:text-white '
             onClick={() => onClick(option)}
         >
-            <div className='m-1'>{option.render ? option.render() : option.value}</div>
+            <span className='text-sm'>{option.render ? option.render() : option.value}</span>
             {/* {option.value} */}
         </li>
     )
 }
 
-const OptionList = ({ options, handleChange }) => {
+const OptionList = ({ options, handleChange, className }) => {
     return (
-        <ul className='z-10 absolute mt-2 w-full uppercase border rounded border-primary-300 bg-white py-4 max-h-80 overflow-y-auto dark:bg-slate-700 dark:border-slate-600'>
+        <ul
+            className={`z-10 absolute transition-all mt-2 w-full capitalize border rounded border-primary-300 bg-white py-1 max-h-80 overflow-y-auto dark:bg-secondary-900 dark:border-secondary-700 ${className}`}
+        >
             {options.map(option => (
                 <Option key={option.id} option={option} onClick={handleChange} />
             ))}
@@ -37,17 +39,20 @@ const OptionList = ({ options, handleChange }) => {
 }
 
 const Select = ({ placeholder, selectedValue, isOpen, btnClasses, ref }) => {
+    const chevronIconClassname = isOpen ? 'rotate-180' : 'rotate-0'
     return (
         <button
             ref={ref}
             type='button'
-            className={`flex justify-between items-center w-full p-1  ${btnClasses} `}
+            className={`flex justify-between capitalize items-center w-full p-1  ${btnClasses} `}
         >
-            <span className={`px-5 uppercase ${selectedValue ?? 'text-slate-400 '}`}>
+            <span className={`px-3  ${selectedValue ?? 'text-slate-400 '}`}>
                 {selectedValue ?? placeholder}
             </span>
 
-            <span className='px-4'>{isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}</span>
+            <span className='px-3'>
+                <ChevronDownIcon className={`w-3 h-3 transition-transform ${chevronIconClassname}`} />
+            </span>
         </button>
     )
 }
@@ -60,17 +65,22 @@ const CustomSelect = ({ label, placeholder, selectedValue, type, ...optionsProps
 
     const { elemRef } = useClickOutsideElement(() => setIsOpen(false))
 
+    const opacityOptionList = isOpen ? 'opacity-1 scale-100' : 'opacity-0 scale-90 '
     return (
-        <div className='flex flex-col'>
+        <div className='flex flex-col '>
             {label && <Label>{label}</Label>}
-            <div ref={elemRef} className={`relative ${containerClasses}`} onClick={handleClick}>
+            <div
+                ref={elemRef}
+                className={`relative min-w-60 first-letter:${containerClasses}`}
+                onClick={handleClick}
+            >
                 <Select
                     placeholder={placeholder}
                     selectedValue={selectedValue}
                     isOpen={isOpen}
                     btnClasses={btnClasses}
                 />
-                {isOpen && <OptionList {...optionsProps} />}
+                <OptionList {...optionsProps} className={opacityOptionList} />
             </div>
         </div>
     )
