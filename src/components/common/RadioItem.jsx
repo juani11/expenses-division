2c72f29899c75cc965ac7387f99e7d22ee59e73f
juani11 <juanitagliero@gmail.com>
2024-09-03 17:17:13 -0300

    Update RadioItem.jsx

--- src/components/common/RadioItem.jsx
+++ src/components/common/RadioItem.jsx
@@ -6,24 +6,46 @@
 
 import { CheckIcon } from '../icons/icons'
 
-const RadioItem = ({ item, selectedValue, onClick, withCheckIcon, children }) => {
-    const { id, description } = item
-    return (
-        <>
-            <div
-                className={`border rounded cursor-pointer relative hover:bg-gray-50 dark:hover:bg-slate-600 
-                ${selectedValue === id ? 'border-primary-500' : 'dark:border-slate-700 '}
-                `}
-                onClick={() => onClick(id)}
-            >
-                {children}
+const radioColors = {
+    primary: 'border-primary',
+    secondary: 'border-secondary'
+}
+const radioIconColors = {
+    primary: 'text-primary',
+    secondary: 'text-secondary'
+}
 
-                {selectedValue === id && withCheckIcon && (
-                    <CheckIcon className=' fill-primary absolute right-4 top-2' />
-                )}
-                {description && <p className='text-xs pb-4 pl-4 ml-0.5 my-2 '>{description}</p>}
-            </div>
-        </>
+const RadioItem = ({
+    item,
+    selectedValue,
+    onClick,
+    withCheckIcon,
+    color = 'secondary',
+    className,
+    children
+}) => {
+    const { id, description } = item
+    const radioColor = radioColors[color]
+    const radioIconColor = radioIconColors[color]
+
+    return (
+        <div
+            className={`text-sm min-h-9 border rounded cursor-pointer relative hover:bg-gray-50 dark:hover:bg-slate-600  ${className}
+                ${selectedValue === id ? radioColor : 'dark:border-slate-700 '}
+                `}
+            onClick={() => onClick(id)}
+        >
+            {children}
+
+            {selectedValue === id && withCheckIcon && (
+                <div className={`absolute right-4 top-2 ${radioIconColor}`}>
+                    <CheckIcon />
+                </div>
+            )}
+            {description && (
+                <p className='text-xs pb-4 pl-4 ml-0.5 my-2 opacity-75 font-light'>{description}</p>
+            )}
+        </div>
     )
 }
 export default RadioItem
