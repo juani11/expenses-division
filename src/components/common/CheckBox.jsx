bb12a3433fae1f1395ae220d8a2a3dc7c16187e6
juani11 <juanitagliero@gmail.com>
2024-09-03 17:16:59 -0300

    Update CheckBox.jsx

--- src/components/common/CheckBox.jsx
+++ src/components/common/CheckBox.jsx
@@ -1,9 +1,15 @@
-const checkBoxClassName = 'cursor-pointer accent-primary border-gray-300 rounded h-4 w-4 '
+const checkBoxClassName = 'cursor-pointer h-4 w-4 '
 
-const CheckBox = ({ label, ...rest }) => {
+const checkBoxColors = {
+    primary: 'accent-primary',
+    secondary: 'accent-secondary '
+}
+const CheckBox = ({ label, color = 'secondary', ...rest }) => {
+    const chkColor = checkBoxColors[color]
+
     return (
         <div className='flex items-center gap-3 '>
-            <input type='checkbox' className={checkBoxClassName} {...rest} />
+            <input type='checkbox' className={`cursor-pointer h-4 w-4 ${chkColor}`} {...rest} />
             <span className='leading-none text-md'>{label}</span>
         </div>
     )
