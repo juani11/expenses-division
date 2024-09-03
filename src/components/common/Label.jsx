574a3da8956f2c52398dd6c2e682e6b6d9a291ed
juani11 <juanitagliero@gmail.com>
2024-09-03 17:17:07 -0300

    Update Label.jsx

--- src/components/common/Label.jsx
+++ src/components/common/Label.jsx
@@ -1,6 +1,9 @@
 const Label = ({ children }) => {
     return (
-        <label htmlFor='name' className='text-md text-gray-800 mb-2 dark:text-white'>
+        <label
+            htmlFor='name'
+            className='text-sm leading-none first-letter:capitalize peer-disabled:cursor-not-allowed peer-disabled:opacity-75 mb-2 dark:text-white'
+        >
             {children}
         </label>
     )
