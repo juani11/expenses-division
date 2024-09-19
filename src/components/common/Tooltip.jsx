e4285fb9d93182f6cdc3336a89d7e7511a3ef796
juani11 <juanitagliero@gmail.com>
2024-09-19 08:54:37 -0300

    Update Tooltip.jsx

--- src/components/common/Tooltip.jsx
+++ src/components/common/Tooltip.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { CloseIcon, TooltipTriangleIcon } from '../icons/icons'
 import Button from './Button'
+import { BOTTOM, RIGHT, TOP } from '../../constants'
 
 const Tooltip = ({
     title,
@@ -8,7 +9,7 @@ const Tooltip = ({
     component: Component,
     withButtons = true,
     closable,
-    position = 'right'
+    position = RIGHT
 }) => {
     const [show, setShow] = useState(false)
     const [loading, setLoading] = useState(false)
@@ -28,43 +29,59 @@ const Tooltip = ({
         }
     }
 
-    const positionClassnames = position === 'right' ? 'right-0  ' : 'ml-4'
+    const positionClassnames =
+        position === RIGHT
+            ? 'left-10'
+            : position === BOTTOM
+            ? 'top-10'
+            : position === TOP
+            ? 'bottom-10'
+            : 'right-10'
+
+    const showClassname = show ? ' scale-100 opacity-1 z-10' : 'scale-90 opacity-0 z-0'
+
+    const tooltipTriangleIconClassname =
+        position === RIGHT
+            ? '-left-2 bottom-0 top-0 my-auto '
+            : position === BOTTOM
+            ? '-top-2 left-0 right-0 mx-auto rotate-90 '
+            : position === TOP
+            ? '-bottom-2 left-0 right-0 mx-auto -rotate-90'
+            : '-right-2 bottom-0 top-0 my-auto -rotate-180 '
 
     return (
         <a
             tabIndex='0'
             role='link'
             aria-label='tooltip 1'
-            className='focus:outline-none focus:ring-gray-300 rounded-full relative flex justify-center items-center'
+            className='focus:outline-none righ focus:ring-gray-300 rounded-full relative w-max flex justify-center items-center'
         >
             <Component onClick={handleClick} />
 
-            {show && (
-                <div
-                    className={`z-20 -mt-20 w-64 absolute transition duration-150 ease-in-out shadow-xl bg-white p-4 rounded dark:bg-slate-800 dark:border-slate-600 dark:border  ${positionClassnames}`}
-                >
-                    <TooltipTriangleIcon />
-                    <p className='text-md font-bold text-gray-800 dark:text-white leading-5 pt-4 '>{title}</p>
-                    {withButtons && (
-                        <div className='flex justify-between mt-4'>
-                            <Button size='sm' onClick={hideTooltip}>
-                                Cancelar
-                            </Button>
-                            <Button size='sm' onClick={onOk} loading={loading} color='primary'>
-                                Ok
-                            </Button>
-                        </div>
-                    )}
-                    {closable && (
-                        <button
-                            className='absolute top-1 right-2 px-1 py-1 rounded hover:bg-gray-100 dark:hover:bg-slate-700'
-                            onClick={hideTooltip}
-                        >
-                            <CloseIcon color='black' width={18} height={18} />
-                        </button>
-                    )}
-                </div>
-            )}
+            <div
+                className={`w-64 h-fit absolute transition shadow-xl bg-white p-4 rounded-lg dark:bg-slate-800 dark:border-slate-600 dark:border ${positionClassnames} ${showClassname}`}
+            >
+                <TooltipTriangleIcon className={`absolute ${tooltipTriangleIconClassname}`} />
+                <p className='text-xs font-semibold text-gray-800 dark:text-white leading-5 '>{title}</p>
+                {withButtons && (
+                    <div className='flex justify-end gap-2 mt-4'>
+                        <Button size='xs' color='secondary' variant='light' onClick={hideTooltip}>
+                            Cancelar
+                        </Button>
+                        <Button size='xs' variant='light' color='primary' onClick={onOk} loading={loading}>
+                            Ok
+                        </Button>
+                    </div>
+                )}
+                {closable && (
+                    <button
+                        className='absolute top-1 right-2 px-1 py-1 rounded hover:bg-gray-100 dark:hover:bg-slate-700'
+                        onClick={hideTooltip}
+                    >
+                        <CloseIcon color='black' width={18} height={18} />
+                    </button>
+                )}
+            </div>
         </a>
     )
 }
