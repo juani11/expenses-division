ae35a13c4c0110b8f6815d2e57f48a9155f92148
juani11 <juanitagliero@gmail.com>
2024-09-19 08:54:03 -0300

    Update ui of Pagination

--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -1,22 +1,70 @@
-import { ChevronLeftBtn, ChevronRightBtn } from './ChevronBtn/chevronBtn'
+import { ArrowLefttIcon, ArrowRightIcon } from '../icons/icons'
+import Button from './Button'
+
+const PaginationButton = ({ onClick, children }) => {
+    return (
+        <Button
+            size='icon'
+            color='secondary'
+            variant='light'
+            onClick={onClick}
+            className={`bg-gray-50 hover:bg-gray-100`}
+        >
+            {children}
+        </Button>
+    )
+}
+const PrevButton = ({ onClick }) => {
+    return (
+        <PaginationButton onClick={onClick}>
+            <ArrowLefttIcon className={'w-4 h-4  '} />
+        </PaginationButton>
+    )
+}
+const NextButton = ({ onClick }) => {
+    return (
+        <PaginationButton onClick={onClick}>
+            <ArrowRightIcon className={'w-4 h-4'} />
+        </PaginationButton>
+    )
+}
 
 const Pagination = ({ cantPages, currentPage, prevPage, nextPage, changeCurrentPage }) => {
+    const percentage = 100 * currentPage
+    // const translate = percentage
+
+    const showPrevButton = currentPage !== 1
+    const showNextButton = currentPage < cantPages
     return (
-        <div className='flex justify-end items-center gap-2 mt-4 '>
-            {currentPage > 1 && <ChevronLeftBtn onClick={prevPage} />}
-            {[...Array(cantPages)].map((_, index) => (
-                <button
-                    key={index}
-                    className={`px-3 py-0.5 rounded hover:bg-gray-100 dark:hover:bg-slate-700 ${
-                        index + 1 === currentPage && 'bg-gray-100 dark:bg-slate-700 font-bold'
-                    }  `}
-                    onClick={changeCurrentPage}
-                    value={index + 1}
-                >
-                    {index + 1}
-                </button>
-            ))}
-            {currentPage < cantPages && <ChevronRightBtn onClick={nextPage} />}
+        <div className='relative flex items-center gap-1'>
+            <div className='h-8 w-8'>{showPrevButton && <PrevButton onClick={prevPage} />}</div>
+
+            {/* <div
+                className={`absolute h-8 left-0 right-0 top-0 bottom-0 mt-auto mb-auto bg-primary dark:bg-gray-600 rounded transition-transform`}
+                style={{
+                    width: '32px',
+                    transform: `translateX(${translate}%)`
+                }}
+            ></div> */}
+            <ul className='flex items-center gap-1'>
+                {[...Array(cantPages)].map((_, index) => (
+                    <li
+                        key={index}
+                        size='xs'
+                        color='secondary'
+                        onClick={changeCurrentPage}
+                        value={index + 1}
+                        className={`h-8 w-8 py-1 px-1 flex justify-center items-center rounded text-xs cursor-pointer z-10  ${
+                            index + 1 === currentPage
+                                ? 'bg-primary-50 text-primary hover:bg-primary-100 font-bold transition-all'
+                                : 'bg-gray-50 hover:bg-gray-100'
+                        }`}
+                    >
+                        {index + 1}
+                    </li>
+                ))}
+            </ul>
+            {showNextButton && <NextButton onClick={nextPage} />}
         </div>
     )
 }
