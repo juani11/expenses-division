c3c429a7e1ea38dbe5e0d0bd1649a62641e5392e
juani11 <juanitagliero@gmail.com>
2024-09-26 10:26:03 -0300

    Update GroupHeader.jsx

--- src/components/group/GroupHeader.jsx
+++ src/components/group/GroupHeader.jsx
@@ -1,69 +1,70 @@
-import { totalAmountOfExpenses } from '../../logic/logic'
+import { useState } from 'react'
+import { totalCashAmountOfExpenses, totalCreditAmountOfExpenses } from '../../logic/logic'
 import { useGroupStore } from '../../store/store'
 import { currencyFormat } from '../../utils/utils'
-import Avatar from '../common/Avatar'
-import { MoneyIllustration, PeopleSelfieIllustration } from '../illustrations/Illustrations'
+import Card2 from '../common/Card2'
+import Tabs from '../common/Tabs'
 import GroupHeaderLoading from './GroupHeaderLoading'
-import GroupHeaderOtions from './GroupHeaderOptions'
+import AddExpense from './expenses/AddExpense'
 
-const GroupHeaderInfo = ({ groupName = 'viaje brasil', cantPersons = 1 }) => {
+const Amount = ({ amount, key }) => {
+    return (
+        <div className='relative'>
+            <h3 className='absolute top-2 font-semibold'>$</h3>
+            <h1 className={`px-4 text-3xl font-semibold animate-fadeLeft`} key={key}>
+                {amount}
+            </h1>
+        </div>
+    )
+}
+
+const TotalAmount = () => {
     const expenses = useGroupStore(state => state.expenses)
+    const totalCreditAmount = currencyFormat(totalCreditAmountOfExpenses(expenses))
+    const totalCashAmount = currencyFormat(totalCashAmountOfExpenses(expenses))
 
-    const totalAmount = currencyFormat(totalAmountOfExpenses(expenses))
+    const [currentSelected, setCurrentSelected] = useState(0)
+    const handleCurrentSelected = selected => setCurrentSelected(selected)
+    const currentSelectedAmount = currentSelected === 0 ? totalCashAmount : totalCreditAmount
 
     return (
         <>
-            <div className='flex items-center justify-center gap-5 mt-6'>
-                <Avatar color='primary'>{groupName?.charAt(0).toUpperCase()}</Avatar>
-                <div className='flex flex-col items-start gap-2 '>
-                    <h2 className='m-0 capitalize'>{groupName}</h2>
-                    <GroupHeaderOtions />
-                </div>
-            </div>
-
-            <div className='flex items-center justify-between gap-10 mt-6 w-96'>
-                <div className='flex flex-col items-center'>
-                    <h4 className='m-0'>Total Gastos</h4>
-                    <div className='flex justify-start items-center'>
-                        <div className='shrink-0'>
-                            <MoneyIllustration width={80} height={50} />
-                        </div>
-                        <div className='relative'>
-                            <h3 className='absolute top-2 '>$</h3>
-                            <h1 className='px-4'>{totalAmount}</h1>
-                        </div>
-                    </div>
-                </div>
-
-                <div className='flex flex-col items-center  m-auto'>
-                    <h4 className='m-0'>Personas </h4>{' '}
-                    <div className='flex justify-between items-center gap-4 '>
-                        <h1 className='grow'>{cantPersons}</h1>
-                    </div>
-                </div>
-            </div>
+            <Tabs
+                tabsNames={['efectivo', 'credito']}
+                currentSelected={currentSelected}
+                handleCurrentSelected={handleCurrentSelected}
+                className={'absolute top-4 right-3 w-36'}
+            />
+            <section className='flex flex-col'>
+                <h4 className='text-sm'>Total Gastos</h4>
+                <Amount amount={currentSelectedAmount} key={currentSelected} />
+            </section>
+        </>
+    )
+}
+const GroupHeaderInfo = ({ groupName }) => {
+    return (
+        <>
+            <section>
+                <h1 className='text-xl font-semibold'>{groupName}</h1>
+            </section>
+            <Card2 className={'relative'}>
+                <TotalAmount />
+                <section className='flex items-center gap-4 mt-4'>
+                    <AddExpense />
+                    {/* <Button size='sm' color='secondary' variant='light'>
+                        agregar persona
+                    </Button> */}
+                </section>
+            </Card2>
         </>
     )
 }
 
-const GroupHeaderImage = () => {
-    return (
-        <div className='hidden md:block md:absolute md:right-0 lg:right-80 '>
-            <PeopleSelfieIllustration width={350} height={300} />
-        </div>
-    )
-}
 const GroupHeader = props => {
     const loading = useGroupStore(state => state.loading)
 
-    return (
-        <div className='flex flex-col-reverse items-center md:flex-row md:relative '>
-            <div className='flex flex-col gap-3 '>
-                {loading ? <GroupHeaderLoading /> : <GroupHeaderInfo {...props} />}
-            </div>
-            <GroupHeaderImage />
-        </div>
-    )
+    return loading ? <GroupHeaderLoading /> : <GroupHeaderInfo {...props} />
 }
 
 export default GroupHeader
