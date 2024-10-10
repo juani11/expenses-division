d11dc0030e282376c342b728ec1afeef63c72f69
juani11 <juanitagliero@gmail.com>
2024-10-10 17:41:24 -0300

    Update DivisionsListContainer.jsx

--- src/components/group/divisions/DivisionsListContainer.jsx
+++ src/components/group/divisions/DivisionsListContainer.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Card2 from '../../common/Card2'
+import Card from '../../common/Card'
 import Tabs from '../../common/Tabs'
 import CashDivisions from './CashDivisions'
 import CreditDivisions from './CreditDivisions'
@@ -10,7 +10,7 @@ const DivisionsListContainer = () => {
 
     const gridRows = currentSelected === 1 ? '40px_40px_auto' : '40px_auto'
     return (
-        <Card2 className={`relative grid grid-rows-[${gridRows}] gap-4 row-span-2 items-start w-full`}>
+        <Card className={`relative grid grid-rows-[${gridRows}] gap-4 row-span-2 items-start w-full`}>
             <Tabs
                 tabsNames={['efectivo', 'credito']}
                 className={'absolute top-4 right-3 w-36'}
@@ -23,7 +23,7 @@ const DivisionsListContainer = () => {
             </section>
 
             {currentSelected === 0 ? <CashDivisions /> : <CreditDivisions />}
-        </Card2>
+        </Card>
     )
 }
 
